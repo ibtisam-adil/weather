8654a97e49cc5f9ea4f932d80f4f6d9b871f3cea
ibtisam-adil <ibtisamadil273@gmail.com>
2024-05-09 10:10:31 +0500

    Navbar UI

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './navbar.css';
+import { MdOutlineStackedLineChart } from 'react-icons/md';
+// import rain from '../../assets/rain.png';
+
+const Navbar = () => (
+  <nav>
+    <div className="nav-head">
+      <MdOutlineStackedLineChart />
+      <h3 className="nav-name">Weather360</h3>
+    </div>
+    <div className="input-with-icon">
+      <input type="text" name="search" placeholder="Search City" required />
+      <i className="bx bx-search-alt-2 input-icon" />
+      <i className="bx bxs-microphone" />
+    </div>
+  </nav>
+);
+
+export default Navbar;
